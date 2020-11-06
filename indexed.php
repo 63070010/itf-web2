76b1f97729e404488dfd841f31a35e0ac9a052a6
63070010 <73679810+63070010@users.noreply.github.com>
2020-11-06 17:18:56 +0700

    Update indexed.php

--- indexed.php
+++ indexed.php
@@ -19,7 +19,7 @@
                         <img src="gambit.jpg" class="img-fluid rounded" />
                     </div>
                     <div class="card-body">
-                        <p>Best series<br>
+                        <p><b>Best series</b>
                            When you read the words “Netflix limited drama series about addiction, obsession, trauma, and chess,” 
                            the first adjective which springs to mind is probably not “thrilling.” But here we are, and “The Queen’s Gambit,”</p>
                     </div>
