cd0b9fe92a385f7a01dffed1c2b8caa4db621f03
63070010 <73679810+63070010@users.noreply.github.com>
2020-11-13 10:42:37 +0700

    Update and rename show.php to databases.php

--- databases.php
+++ databases.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'itf-lab12.mysql.database.azure.com', 'it63070010@itf-lab12', 'PJPvjx84', 'itf-lab12', 3306);
+mysqli_real_connect($conn, 'itf-lab12.mysql.database.azure.com', 'it63070010@itf-lab12', 'PJPvjx84', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
